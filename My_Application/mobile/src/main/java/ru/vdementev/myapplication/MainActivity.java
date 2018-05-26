4253850d75e361e3841d5835ea821a027047d345
Dementyev Vladimir <sayman000111@gmail.com>
2018-05-26 22:24:46 +0300

    Урок 2

--- My_Application/mobile/src/main/java/ru/vdementev/myapplication/MainActivity.java
+++ My_Application/mobile/src/main/java/ru/vdementev/myapplication/MainActivity.java
@@ -1,15 +1,28 @@
 package ru.vdementev.myapplication;
 
 import android.app.Activity;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+
+    private TextView textView;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mainlayout);
+
+        textView  = (TextView) findViewById(R.id.textView);
     }
+
+    public void sayHello(View view){
+
+        textView.setText("Новое значение 1");
+
+    }
+
 }
