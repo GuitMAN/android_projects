b8849b7a1fab9e31dc00fde638d1f2d76f7f1ae9
Dementyev Vladimir <sayman000111@gmail.com>
2018-05-26 16:57:26 +0300

    Урок 1

---
+++ My_Application/mobile/src/main/java/ru/vdementev/myapplication/MainActivity.java
@@ -0,0 +1,15 @@
+package ru.vdementev.myapplication;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mainlayout);
+    }
+}
